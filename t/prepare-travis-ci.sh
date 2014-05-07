34a794f83539a15c20fb17c6cd0c8bc4ecc3be67
Kang-min Liu <gugod@gugod.org>
2014-05-07 08:52:34 +0200

    print the elasticsearch welcome message

--- t/prepare-travis-ci.sh
+++ t/prepare-travis-ci.sh
@@ -4,3 +4,6 @@ cd /tmp
 
 curl --silent -L http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz | tar -xz
 ./elasticsearch-${ES_VERSION}/bin/elasticsearch
+
+sleep 10;
+curl http://localhost:9200/
