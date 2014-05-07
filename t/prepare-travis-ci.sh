ada451f7ce1969c123360a9c1c0bffc39cb3076b
Kang-min Liu <gugod@gugod.org>
2014-05-07 08:58:05 +0200

    ditto

--- t/prepare-travis-ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-cd /tmp
-
-curl --silent -L http://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz | tar -xz
-./elasticsearch-${ES_VERSION}/bin/elasticsearch
-
-sleep 10;
-curl http://localhost:9200/
